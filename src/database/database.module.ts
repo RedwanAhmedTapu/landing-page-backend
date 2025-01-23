2a3d1019f5911e08bb14780c81a40c12311e2794
RedwanAhmedTapuu <redwan25880@gmail.com>
2025-01-24 01:25:30 +0600

    mongodburl updated

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/your-database-name'),
+    MongooseModule.forRoot('mongodb+srv://landingpage:<landing2450>@cluster0.am6vh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
   ],
 })
 export class DatabaseModule {}
